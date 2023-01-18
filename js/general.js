533069630de4756dad7892c7f3ba641138876c89
AJ-Mareco <loarregloyomismo@gmail.com>
2023-01-18 09:52:31 -0300

    se agrego la funcionalidad al html

--- js/general.js
+++ js/general.js
@@ -1,12 +1,19 @@
-var contador = 0;
+var contadorEstudios = 0;
+var contadorExperiencia = 0;
+var contadorHerramientas = 0;
+var contadorConocimientos = 0;
 
-function ocultar(){
+function ocultar(contador, idElemento){
     if(contador==0){
-        document.getElementById("estudios").classList.add("oculto");
+        
+        document.getElementById(idElemento).classList.add("oculto");
         contador++;
     }else{
-        document.getElementById("estudios").classList.remove("oculto");
+        document.getElementById(idElemento).classList.remove("oculto");
         contador = 0;
     }
 }
-document.getElementById("a-estudios").addEventListener('click',ocultar,true);
+document.getElementById("a-estudios").addEventListener('click',ocultar(contadorEstudios, 'estudios'),true);
+document.getElementById("a-estudios").addEventListener('click',ocultar(contadorHerramientas, 'herramientas'),true);
+document.getElementById("a-estudios").addEventListener('click',ocultar(contadorConocimientos, 'conocimientos'),true);
+document.getElementById("a-estudios").addEventListener('click',ocultar(contadorExperiencia, 'experiencia'),true);
